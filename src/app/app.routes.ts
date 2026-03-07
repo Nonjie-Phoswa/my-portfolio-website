0fb35957ed4e72910979232bfba679ffc03b2a09
nophoswjhb024 <nonjie.phoswa@gmail.com>
2026-03-07 23:33:14 +0200

    configured the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home';
+import { AboutComponent } from './pages/about/about';
+import { ProjectsComponent } from './pages/projects/projects';
+import { CertificatesComponent } from './pages/certificates/certificates';
+import { ExperienceComponent } from './pages/experience/experience';
+import { EducationComponent } from './pages/education/education';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'certificates', component: CertificatesComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'education', component: EducationComponent },
+];

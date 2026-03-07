67d3cfaa87d856f5d61751b7a8b82b6259c38fca
nophoswjhb024 <nonjie.phoswa@gmail.com>
2026-03-07 23:42:39 +0200

    fixed standalone component imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home';
-import { AboutComponent } from './pages/about/about';
-import { ProjectsComponent } from './pages/projects/projects';
-import { CertificatesComponent } from './pages/certificates/certificates';
-import { ExperienceComponent } from './pages/experience/experience';
-import { EducationComponent } from './pages/education/education';
+import { Home} from './pages/home/home';
+import { About} from './pages/about/about';
+import { Projects } from './pages/projects/projects';
+import { Certificates } from './pages/certificates/certificates';
+import { Experience } from './pages/experience/experience';
+import { Education } from './pages/education/education';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'certificates', component: CertificatesComponent },
-  { path: 'experience', component: ExperienceComponent },
-  { path: 'education', component: EducationComponent },
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: 'certificates', component: Certificates },
+  { path: 'experience', component: Experience },
+  { path: 'education', component: Education },
 ];

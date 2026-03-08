69ddb3ba55a9d5c1a7abd1710792432b0d68db61
nophoswjhb024 <nonjie.phoswa@gmail.com>
2026-03-08 23:36:39 +0200

    imported library for typing/deleting animation

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,53 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css',
 })
-export class Home {
-  roles: string[] = [
-    'Web Developer',
-    'Frontend Developer',
-    'Mobile Developer',
-    'Software Engineer',
-  ];
+export class Home implements AfterViewInit {
+  ngAfterViewInit() {
+    const options = {
+      strings: ['Web Developer', 'Frontend Developer', 'Mobile Developer', 'Software Engineer'],
+      typeSpeed: 80,
+      backSpeed: 40,
+      backDelay: 1500,
+      loop: true,
+    };
 
-  currentRoleIndex = 0;
-  displayedText = '';
-  isDeleting = false;
-
-  typingSpeed = 100;
-
-  constructor() {
-    this.type();
-  }
-
-  type() {
-    const fullText = this.roles[this.currentRoleIndex];
-
-    if (this.isDeleting) {
-      this.displayedText = fullText.substring(0, this.displayedText.length - 1);
-    } else {
-      this.displayedText = fullText.substring(0, this.displayedText.length + 1);
-    }
-
-    let speed = this.typingSpeed;
-
-    if (this.isDeleting) {
-      speed /= 2;
-    }
-
-    if (!this.isDeleting && this.displayedText === fullText) {
-      speed = 1500;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.displayedText === '') {
-      this.isDeleting = false;
-      this.currentRoleIndex = (this.currentRoleIndex + 1) % this.roles.length;
-    }
-
-    setTimeout(() => this.type(), speed);
+    new Typed('#typed-text', options);
   }
 }

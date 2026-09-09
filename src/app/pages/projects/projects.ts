988a0d88a4cb542464069e4ed06fb495f03516ed
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-09-09 16:24:55 +0200

    projects page in construction: added 3 projects

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
   templateUrl: './projects.html',
-  styleUrl: './projects.css',
+  styleUrls: ['./projects.css']
 })
-export class Projects {
+export class Projects implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngAfterViewInit() {
+    // Initialize AOS animations if you're using them
+    if (isPlatformBrowser(this.platformId)) {
+      // AOS initialization would go here if you're using it
+      // import AOS from 'aos';
+      // AOS.init({
+      //   duration: 800,
+      //   once: true,
+      //   offset: 100
+      // });
+    }
+  }
 }

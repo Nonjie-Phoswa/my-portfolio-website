d764d0c652495ee286df72659773d6229cd07083
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-09-09 13:00:32 +0200

    new navbar design

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener, OnInit, inject } from '@angular/core';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -7,8 +8,31 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements OnInit {
   isMenuOpen = false;
+  isScrolled = false;
+
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.checkScroll();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.closeMenu();
+      }
+    });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkScroll();
+  }
+
+  private checkScroll() {
+    if (typeof window !== 'undefined') {
+      this.isScrolled = window.scrollY > 20;
+    }
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -18,3 +42,4 @@ export class Navbar {
     this.isMenuOpen = false;
   }
 }
+
